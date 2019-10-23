4559e9b8bbc71a802eb676384f4206413fb327af
Andrew <andrewvu.cs@gmail.com>
2019-10-23 13:46:59 -0700

    set up redux store and reducer

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-import ProductsOverviewScreen from './screens/shop/ProductsOverviewScreen';
+import productsReducer from "./store/reducers/products.js";
+import ProductsOverviewScreen from "./screens/shop/ProductsOverviewScreen";
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-      <ProductsOverviewScreen/>
+    <Provider store={store}>
+      <ProductsOverviewScreen />
+    </Provider>
   );
 }
-
-
