6f61612ac306fb394047ca9465c70146a6398dc8
Andrew <andrewvu.cs@gmail.com>
2019-10-24 15:55:24 -0700

    configuring edit product screen

--- screens/user/UserProductScreen.js
+++ screens/user/UserProductScreen.js
@@ -10,8 +10,14 @@ import Colors from "../../constants/Colors";
 
 const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
-
   const dispatch = useDispatch();
+
+  const editProductHandler = id => {
+    props.navigation.navigate("EditProduct", {
+      productId: id
+    });
+  };
+
   return (
     <FlatList
       data={userProducts}
@@ -21,9 +27,17 @@ const UserProductsScreen = props => {
           imageUrl={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onSelect={() => {}}
+          onSelect={() => {
+            editProductHandler(itemData.item.pid);
+          }}
         >
-          <Button color={Colors.primary} title="Edit" onPress={() => {}} />
+          <Button
+            color={Colors.primary}
+            title="Edit"
+            onPress={() => {
+              editProductHandler(itemData.item.pid);
+            }}
+          />
           <Button
             color={Colors.primary}
             title="Delete"
@@ -47,6 +61,17 @@ UserProductsScreen.navigationOptions = navData => {
           iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
           onPress={() => {
             navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === "android" ? "md-create" : "ios-create"}
+          onPress={() => {
+            navData.navigation.navigate("EditProduct");
           }}
         />
       </HeaderButtons>
