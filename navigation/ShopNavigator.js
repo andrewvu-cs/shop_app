8d156ac4467d002e21db7d08a6c9663fa2eed9f1
Andrew <andrewvu.cs@gmail.com>
2019-10-30 12:55:14 -0700

    implemented a basic login screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { Platform } from "react-native";
 
+import AuthScreen from "../screens/user/AuthScreen";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailsScreen from "../screens/shop/ProductDetailsScreen";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
-import UserProductScreen from '../screens/user/UserProductScreen';
-import EditProductScreen from '../screens/user/EditProductScreen';
+import UserProductScreen from "../screens/user/UserProductScreen";
+import EditProductScreen from "../screens/user/EditProductScreen";
 import Colors from "../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -81,7 +82,7 @@ const AdminNavigator = createStackNavigator(
     },
     defaultNavigationOptions: defaultNavOptions
   }
-)
+);
 
 const ShopNavigator = createDrawerNavigator(
   {
@@ -95,4 +96,18 @@ const ShopNavigator = createDrawerNavigator(
     }
   }
 );
-export default createAppContainer(ShopNavigator);
+
+const AuthNavigator = createStackNavigator(
+  {
+    Auth: AuthScreen
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator
+});
+export default createAppContainer(MainNavigator);
