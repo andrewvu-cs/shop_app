66baadf098972194fe8d5d7434a209012fdf5318
Andrew <andrewvu.cs@gmail.com>
2019-10-31 16:03:22 -0700

    implemented logout button for users

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import { Platform } from "react-native";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
+import { Platform, SafeAreaView, Button, View } from "react-native";
+import { useDispatch } from 'react-redux';
 
 import AuthScreen from "../screens/user/AuthScreen";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
@@ -11,9 +12,10 @@ import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
 import UserProductScreen from "../screens/user/UserProductScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
-import StartupScreen from '../screens/StartupScreen';
+import StartupScreen from "../screens/StartupScreen";
 import Colors from "../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
+import * as authActions from '../store/actions/auth';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -94,6 +96,20 @@ const ShopNavigator = createDrawerNavigator(
   {
     contentOptions: {
       activeTintColor: Colors.primary
+    },
+    contentComponent: props => {
+      const dispatch = useDispatch();
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <SafeAreaView forceInset={{ top: "always", horizontal: "never" }}>
+            <DrawerItems {...props} />
+            <Button title="Logout" color={Colors.primary} onPress={() => {
+              dispatch(authActions.logout());
+              props.navigation.navigate('Auth');
+            }} />
+          </SafeAreaView>
+        </View>
+      );
     }
   }
 );
@@ -108,7 +124,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
-  Startup:StartupScreen,
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator
 });
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,7 +2,8 @@ import {AsyncStorage} from 'react-native';
 
 // export const SIGNUP = 'SIGNUP';
 // export const LOGIN = 'LOGIN';
-export const AUTHENTICATE = 'AUTHENTICATE'
+export const AUTHENTICATE = 'AUTHENTICATE';
+export const LOGOUT = 'LOGOUT';
 
 export const authenticate = (userId, token) => {
   return { type: AUTHENTICATE, userId: userId, token: token};
@@ -81,6 +82,11 @@ export const login = (email, password) => {
     saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
 };
+
+export const logout = () => {
+  return { type: LOGOUT }
+};
+
 
 const saveDataToStorage = (token, userId, expirationDate) => {
   AsyncStorage.setItem('userData', JSON.stringify({
